7bd9c97960e1fd5a75cdf4a5f6f97102a78cf267
azoom-keishi-ichida <keishi.ichida@azoom.jp>
2024-03-05 11:09:46 +0900

    deleteの学習とメソッドチェーンの実験

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ async function main() {
   //   data: [
   //     { name: 'Bob', email: 'bob@prisma.io' },
   //     { name: 'Bobo', email: 'bob@prisma.io' },
-  //     { name: 'Yewande', email: 'yewande@prisma.io'},
+  //     { name: 'Yewande', email: 'yewande@prisma.io' },
   //     { name: 'Angelique', email: 'angelique@prisma.io' },
   //   ],
   //   skipDuplicates: true, // Skip 'Bobo'
@@ -71,7 +71,7 @@ async function main() {
   //   },
   // })
 
-  // emailのupdate 
+  // emailのupdate
   // const updateUser = await prisma.user.update({
   //   where: {
   //     email: 'viola@prisma.io',
@@ -94,21 +94,148 @@ async function main() {
   // })
 
   // whereでデータが存在すればupdateを存在しなければクリエイトを行う
-  const upsertUser = await prisma.user.upsert({
-    where: {
-      email: 'viola@prisma.io',
-    },
-    update: {
-      name: 'Viola the Magnificent',
-    },
-    create: {
-      email: 'viola@prisma.io',
-      name: 'Viola the Magnificent',
-    },
-  })
+  // const upsertUser = await prisma.user.upsert({
+  //   where: {
+  //     email: 'viola@prisma.io',
+  //   },
+  //   update: {
+  //     name: 'Viola the Magnificent',
+  //   },
+  //   create: {
+  //     email: 'viola@prisma.io',
+  //     name: 'Viola the Magnificent',
+  //   },
+  // })
+
+  // const createPosts = await prisma.post.createMany({
+  //   data: [{ published: true, title: 'POST' }],
+  // })
+
+  // インクリメントを行うクエリ
+  // const updatePosts = await prisma.post.updateMany({
+  //   data: {
+  //     views: {
+  //       increment: 1,
+  //     },
+  //     likes: {
+  //       increment: 1,
+  //     },
+  //   },
+  // })
+
+  // 1つのデータを削除するdeleteメソッド
+  // const deleteUser = await prisma.user.delete({
+  //   where: {
+  //     email: 'angelique@prisma.io',
+  //   },
+  // })
+
+  // 複数のデータを一括削除するdeleteManyメソッド
+  // const deleteUsers = await prisma.user.deleteMany({
+  //   where: {
+  //     email: {
+  //       contains: 'prisma.io',
+  //     },
+  //   },
+  // })
+
+  //全てのレコードを削除するdeleteメソッド
+  // const deleteUsers = await prisma.user.deleteMany({})
+
+  // deleteを行う際に、子モデルのデータを削除することができないのでトランザクションを使って削除
+  // const deletePosts = prisma.post.deleteMany({
+  //   where: {
+  //     authorId: 7,
+  //   },
+  // })
+
+  // const deleteUser = prisma.user.delete({
+  //   where: {
+  //     id: 7,
+  //   },
+  // })
+  // const transaction = await prisma.$transaction([deletePosts, deleteUser])
+  // MEMO: onDelete: Cascade をschemaに設定することで上の無駄コード無しで関連削除を行なってくれる
+
+  // const u = await prisma.user.create({
+  //   include: {
+  //     posts: {
+  //       include: {
+  //         categories: true,
+  //       },
+  //     },
+  //   },
+  //   data: {
+  //     email: 'emma@prisma.io',
+  //     posts: {
+  //       create: [
+  //         {
+  //           title: 'My first post',
+  //           categories: {
+  //             connectOrCreate: [
+  //               {
+  //                 create: { name: 'Introductions' },
+  //                 where: {
+  //                   id: 1,
+  //                 },
+  //               },
+  //               {
+  //                 create: { name: 'Social' },
+  //                 where: {
+  //                   name: 'Social',
+  //                   id: 2,
+  //                 },
+  //               },
+  //             ],
+  //           },
+  //         },
+  //         {
+  //           title: 'How to make cookies',
+  //           categories: {
+  //             connectOrCreate: [
+  //               {
+  //                 create: { name: 'Social' },
+  //                 where: {
+  //                   name: 'Social',
+  //                   id: 1,
+  //                 },
+  //               },
+  //               {
+  //                 create: { name: 'Cooking' },
+  //                 where: {
+  //                   name: 'Cooking',
+  //                   id: 2,
+  //                 },
+  //               },
+  //             ],
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   },
+  // })
+
+  // メソッドチェーンを使用(こちらの使用はできない)
+  // const UserWithCategories = await prisma.post
+  //   .findUnique({
+  //     where: {
+  //       id: 3,
+  //     },
+  //   })
+  //   .categories.findMany({})
+
+  // メソッドチェーンの代替案
+  // const UserWithCategories = await prisma.post.findUnique({
+  //   where: {
+  //     id: 3,
+  //   },
+  //   include: {
+  //     categories: true,
+  //   },
+  // })
 
   // コンソールによる確認
-  console.dir(upsertUser)
+  console.dir(UserWithCategories?.categories)
 }
 
 // mainメソッドの実行とエラーの出力
